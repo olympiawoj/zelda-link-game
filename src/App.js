8252e136dcd21df574b86d1266132c20b9d38eac
olympiawoj <owojcik7@gmail.com>
2021-02-13 13:28:32 -0500

    Setting Up Canvas and Box

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
 
 export default function App() {
+  const canvasRef = useRef(null)
+  const [x, setX] = useState(0)
+  const [y, setY] = useState(0)
+
+  // Set the height and width of canvas
+  useEffect(()=>{
+    const context = canvasRef.current.getContext('2d')
+    context.canvas.height = window.innerHeight;
+    context.canvas.width = window.innerWidth;
+
+  },[]) // run only once, upon load
+
+  // Move the box if x or y changes
+  useEffect(()=>{
+    const context = canvasRef.current.getContext('2d')
+    context.clearRect(0, 0, window.innerHeight, window.innerWidth)
+
+    context.fillRect(x, y, 100, 100)
+  }, [x, y]) 
+
+
   return (
     <div className="app">
-      <canvas />
+      <canvas ref={canvasRef} />
 
       <div className="arrows">
-        <button>Up</button>
-        <button>Left</button>
-        <button>Down</button>
-        <button>Right</button>
+        <button onClick={()=>setY(y => y - 20)}>Up</button>
+        <button onClick={()=>setX(x => x - 20)}>Left</button>
+        <button onClick={()=>setY(y => y + 20)}>Down</button> 
+        <button onClick={()=>setX(x => x + 20)}>Right</button>
       </div>
 
       <div className="images">
