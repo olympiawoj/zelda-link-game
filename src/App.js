5f7b251ed9a374c4976d9034498075cf9184f2ba
olympiawoj <owojcik7@gmail.com>
2021-02-14 09:54:43 -0500

    Link Sprites

--- src/App.js
+++ src/App.js
@@ -1,45 +1,57 @@
-import React, {useEffect, useRef, useState} from 'react';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 export default function App() {
-  const canvasRef = useRef(null)
-  const [x, setX] = useState(0)
-  const [y, setY] = useState(0)
+  const canvasRef = useRef(null);
+  const linkDownRef = useRef(null);
+  const linkUpRef = useRef(null);
+  const linkLeftRef = useRef(null);
+  const linkRightRef = useRef(null);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [direction, setDirection] = useState("down");
 
   // Set the height and width of canvas
-  useEffect(()=>{
-    const context = canvasRef.current.getContext('2d')
+  useEffect(() => {
+    const context = canvasRef.current.getContext("2d");
     context.canvas.height = window.innerHeight;
     context.canvas.width = window.innerWidth;
-
-  },[]) // run only once, upon load
+  }, []); // run only once, upon load
 
   // Move the box if x or y changes
-  useEffect(()=>{
-    const context = canvasRef.current.getContext('2d')
-    context.clearRect(0, 0, window.innerHeight, window.innerWidth)
+  useEffect(() => {
+    const context = canvasRef.current.getContext("2d");
+    context.clearRect(0, 0, window.innerHeight, window.innerWidth);
 
-    context.fillRect(x, y, 100, 100)
-  }, [x, y]) 
+    let theLinkRef;
+    if (direction === "down") theLinkRef = linkDownRef;
+    if (direction === "up") theLinkRef = linkUpRef;
+    if (direction === "right") theLinkRef = linkRightRef;
+    if (direction === "left") theLinkRef = linkLeftRef;
+
+    // context.fillRect(x, y, 100, 100)
+    context.drawImage(theLinkRef.current, x, y);
+  }, [x, y]);
 
   // add event listener to window to listen for arrow keys
-  useEffect(()=>{
-    window.addEventListener('keydown',handleKeyDown)
-    function handleKeyDown(e){
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    function handleKeyDown(e) {
       // console.log(e.key)
-      if(e.key === 'ArrowUp') move('up')
-      if(e.key === 'ArrowLeft') move('left')
-      if(e.key === 'ArrowDown') move('down')
-      if(e.key === 'ArrowRight') move('right')
+      if (e.key === "ArrowUp") move("up");
+      if (e.key === "ArrowLeft") move("left");
+      if (e.key === "ArrowDown") move("down");
+      if (e.key === "ArrowRight") move("right");
     }
-    return ()=> window.removeEventListener('keydown', handleKeyDown)
-  }, [])
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
 
-  function move(direction){
-    if(direction === 'up') setY((y) => y-20)
-    if(direction === 'left') setX((x) => x-20)
-    if(direction === 'down') setY((y) => y+20)
-    if(direction === 'right') setX((x) => x+20)
+  function move(dir) {
+    setDirection(dir);
+    if (dir === "up") setY((y) => y - 20);
+    if (dir === "left") setX((x) => x - 20);
+    if (dir === "down") setY((y) => y + 20);
+    if (dir === "right") setX((x) => x + 20);
   }
 
   return (
@@ -47,17 +59,29 @@ export default function App() {
       <canvas ref={canvasRef} />
 
       <div className="arrows">
-        <button onClick={()=>move('up')}>Up</button>
-        <button onClick={()=>move('left')}>Left</button>
-        <button onClick={()=>move('down')}>Down</button> 
-        <button onClick={()=> move('right')}>Right</button>
+        <button onClick={() => move("up")}>Up</button>
+        <button onClick={() => move("left")}>Left</button>
+        <button onClick={() => move("down")}>Down</button>
+        <button onClick={() => move("right")}>Right</button>
       </div>
 
       <div className="images">
-        <img src="https://i.imgur.com/JYUB0m3.png" alt="Down" />
-        <img src="https://i.imgur.com/GEXD7bk.gif" alt="Right" />
-        <img src="https://i.imgur.com/XSA2Oom.gif" alt="Up" />
-        <img src="https://i.imgur.com/4LGAZ8t.gif" alt="Left" />
+        <img
+          ref={linkDownRef}
+          src="https://i.imgur.com/JYUB0m3.png"
+          alt="Down"
+        />
+        <img
+          ref={linkRightRef}
+          src="https://i.imgur.com/GEXD7bk.gif"
+          alt="Right"
+        />
+        <img ref={linkUpRef} src="https://i.imgur.com/XSA2Oom.gif" alt="Up" />
+        <img
+          ref={linkLeftRef}
+          src="https://i.imgur.com/4LGAZ8t.gif"
+          alt="Left"
+        />
       </div>
     </div>
   );
